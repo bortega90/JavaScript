62692049ad6bea7ac1fba4e233d789558257347a
bortega90 <147768691+bortega90@users.noreply.github.com>
2023-11-14 23:31:43 -0800

    Add files via upload

---
+++ JavaScript/Project2_functions/JS/main.js
@@ -0,0 +1,15 @@
+function myFunction () {
+    var str ="hello function!";
+
+    document.getElementById("Button_Text"). innerHTML=str;
+
+}
+
+function myFunction () {
+    var str = "I made my first JS button, ";  //Turns previous sentence into this when clicked 
+    str += " I am learning so much!";
+    document.getElementById("Concatenate").innerHTML= str;
+}
+
+
+document.getElementById("Concatenate").innterHTML = date; //JS to link to HTML for time
