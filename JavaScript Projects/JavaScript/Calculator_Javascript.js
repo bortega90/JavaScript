53e6743975dff4df1ed14497c3c4b3844623aabc
bortega90 <britnyortega@gmail.com>
2023-11-22 14:41:03 -0800

    push fixed Update_Display
    changed typeo('even' should be 'event') in Update_Display line 95

--- JavaScript Projects/JavaScript/Calculator_Javascript.js
+++ JavaScript Projects/JavaScript/Calculator_Javascript.js
@@ -92,7 +92,7 @@ function Update_Display() {
 Update_Display();
 //this section monitors button clicks
 const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (even) => {
+keys.addEventListener('click', (event) => {
     //the target variable is an object that represents the element
     //that was clicked
     const { target } = event;
